a55ba03b86518448c05fb61e147080e4783606a5
donner1david2 <147050522+donner1david2@users.noreply.github.com>
2026-02-03 18:25:25 +0100

    Update calculator.py
    c

--- calculator.py
+++ calculator.py
@@ -1,7 +1,7 @@
 # calculator.py
 def add(x, y):
-print(f"\n--- DEBUG: Calculating {x} + {y} ---")
+ print(f"\n--- DEBUG: Calculating {x} + {y} ---")
  return x + y
 def subtract(x, y):
-print(f"\n--- DEBUG: Calculating {x} - {y} ---")
+ print(f"\n--- DEBUG: Calculating {x} - {y} ---")
  return x - y
