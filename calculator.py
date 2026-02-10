9151b38941cdbead372ba46a5f1a5b7049580d3e
donner1david2 <147050522+donner1david2@users.noreply.github.com>
2026-02-10 17:49:22 +0100

    abc

--- calculator.py
+++ calculator.py
@@ -1,6 +1,6 @@
 # calculator.py
 def add(x, y):
- return x - y # <--- Oops! I used minus instead of plus
+ return x + y # <--- Oops! I used minus instead of plus
 def subtract(x, y):
  print(f"\n--- DEBUG: Calculating {x} - {y} ---")
  return x - y
